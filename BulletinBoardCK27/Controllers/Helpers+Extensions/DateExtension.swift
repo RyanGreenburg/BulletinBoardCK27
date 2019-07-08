9e50ef9f12eeff5419fde7b3e6cf15fed30e772a
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-07-08 11:31:46 -0600

    adds partial messageViewController & date extension

---
+++ BulletinBoardCK27/Controllers/Helpers+Extensions/DateExtension.swift
@@ -0,0 +1,20 @@
+//
+//  DateExtension.swift
+//  BulletinBoardCK27
+//
+//  Created by RYAN GREENBURG on 7/8/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    func formatDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        
+        return formatter.string(from: self)
+    }
+}
